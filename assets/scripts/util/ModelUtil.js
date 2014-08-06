49e01c3a9791a7a665fc51b34352a7955c62b737
Michael Lin <mlin@twitter.com>
2014-08-06 16:54:22 -0700

    added deep merge to modelutil which merge w protos attrs

--- assets/scripts/util/ModelUtil.js
+++ assets/scripts/util/ModelUtil.js
@@ -6,8 +6,22 @@ define([
       return typeof(cls) === 'string' ? require(cls) : cls;
     },
 
-    mergeOption: function(name, model, options) {
-      var result = $.extend(true, {}, model[name] || {}, options[name] || {});
+    mergeOption: function(name, model, options, deep) {
+
+      var args = [options[name] || {}];
+
+      if (deep) {
+        var proto = model;
+        while (proto) {
+          if (proto.hasOwnProperty(name)) {
+            args.unshift(proto[name]);
+          }
+          proto = Object.getPrototypeOf(proto);
+        }
+      } else {
+        args.unshift(model[name] || {});
+      }
+      var result = $.extend.apply(null, [true, {}].concat(args));
       delete options[name];
       model[name] = result;
     }
